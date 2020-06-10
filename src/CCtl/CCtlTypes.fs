3995479a60fde5682ecd2e91b8e20d07b867ff2e
Frederik Jacobsen <fkjacobsen@gmail.com>
2020-06-10 16:53:54 +0200

    Add basic AST for c-CTL.

---
+++ src/CCtl/CCtlTypes.fs
@@ -0,0 +1,27 @@
+namespace CCtl
+
+module CCtlTypes =
+
+    type State = string
+    type Valuation<'D> = State -> 'D
+
+    /// A formula in c-CTL over domain 'D is recursively defined over the following cases:
+    type Formula<'D> =
+        | Zero
+        | One
+        | Variable of string
+        | Proposition of string
+        | Function of string * Formula<'D> list
+        | Choose of Formula<'D> * Formula<'D>
+        | Combine of Formula<'D> * Formula<'D>
+        | Quantifier of Quantifier * Temporal<'D>
+        | Next of Quantifier * Formula<'D>
+    and Quantifier =
+        | Glb
+        | Sum
+        | Product
+    and Temporal<'D> =
+        | Temporal of Formula<'D> * Op * Op * Formula<'D>
+    and Op =
+        | Choose
+        | Combine
--- src/CCtl/Library.fs
+++
@@ -1,5 +0,0 @@
-﻿namespace CCtl
-
-module Say =
-    let hello name =
-        printfn "Hello %s" name
